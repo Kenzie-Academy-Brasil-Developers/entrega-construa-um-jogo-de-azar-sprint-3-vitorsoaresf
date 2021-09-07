aa522cbd5a4dcc1090dcc6fdf91f942a1f93699d
vitorsoaresf <vitorsoaresf@gmail.com>
2021-09-07 13:50:58 -0300

    implementa preenchimento da tabela

--- script/script.js
+++ script/script.js
@@ -31,7 +31,7 @@ const createTable = () => {
         const tr = document.createElement('tr');
         for (let z = 0; z < 10; z++) {
             const td = document.createElement('td');
-            td.classList.add('style_td');
+            td.classList.add('td_style');
             td.setAttribute('id', contId);
             contId++;
             tr.appendChild(td);
@@ -49,6 +49,19 @@ const removeIdsLine = (digit) => {
     }
 }
 
+//RETORNA TODAS AS ID'S TD
+const returnAllTD = (arrTd) => {
+    let arr = [];
+    for (let i = 0; i < arrTd.length; i++) {
+        arr.push(Number.parseInt(arrTd[i].getAttribute('id')));
+    }
+    return arr;
+}
+
+const youWin = () => {
+    alert('você achou uma palavra')
+}
+
 // ARRAY DE ANIMAIS
 const zoo = ['abelha', 'andorinha', 'anta', 'boi', 'babuíno', 'baleia', 'cachorro', 'camaleão', 'dromedário', 'dragão', 'ema', 'elefante', 'formiga', 'foca', 'flamingo', 'gato', 'golfinho', 'guaxinim', 'hipopótamo', 'hiena']
 
@@ -57,17 +70,17 @@ createTable();
 
 // CRIANDO ARRAY DOS ID'S  DOS ELEMENTOS <td>
 const arrTd = document.getElementsByTagName('td');
-const arrTdId = [];
-for (let i = 0; i < arrTd.length; i++) {
-    arrTdId.push(Number.parseInt(arrTd[i].getAttribute('id')));
-}
+const arrTdId = returnAllTD(arrTd);
 
-// SELECIONA A PALAVRA E A REMOVE PARA NAO HAVER DUPLICACAO DE PALAVRAS NO TABULEIRO
+// SELECIONA O ANIMAL E A REMOVE PARA NAO HAVER DUPLICACAO DE ANIMAIS NO TABULEIRO
 const beast1 = zoo.splice(Math.floor(Math.random() * zoo.length), 1).toString();
 const beast2 = zoo.splice(Math.floor(Math.random() * zoo.length), 1).toString();
 const beast3 = zoo.splice(Math.floor(Math.random() * zoo.length), 1).toString();
 
+// ARMAZENARA AS POSICOES ONDE ESTAO AS PALAVRAS
+const elementsPositionsWin = [];
 
+// BEAST 1
 let selectId1 = randomPosition(beast1);
 let digit = Number.parseInt(selectId1.toString()[0]) * 10;
 removeIdsLine(digit);
@@ -75,24 +88,45 @@ removeIdsLine(digit);
 
 for (let i = 0; i < beast1.length; i++, selectId1++) {
     const element = document.getElementById(`${selectId1}`);
+    elementsPositionsWin.push(Number.parseInt(element.getAttribute('id')));
     element.innerText = beast1[i];
 }
 
+// BEAST 2
 let selectId2 = randomPosition(beast2);
 digit = Number.parseInt(selectId2.toString()[0]) * 10;
 removeIdsLine(digit);
 
 for (let i = 0; i < beast2.length; i++, selectId2++) {
     const element = document.getElementById(`${selectId2}`);
+    elementsPositionsWin.push(Number.parseInt(element.getAttribute('id')));
     element.innerText = beast2[i];
 }
 
-
+// BEAST 3
 let selectId3 = randomPosition(beast3);
 digit = Number.parseInt(selectId3.toString()[0]) * 10;
 removeIdsLine(digit);
 
 for (let i = 0; i < beast3.length; i++, selectId3++) {
     const element = document.getElementById(`${selectId3}`);
+    elementsPositionsWin.push(Number.parseInt(element.getAttribute('id')));
     element.innerText = beast3[i];
+}
+
+let arrCompleteTable = returnAllTD(arrTd);
+
+// INSERINDO OUTRAS LETRAS NO ESPACOS VAGOS
+for (let i = 0; i < arrCompleteTable.length; i++) {
+    if (!(elementsPositionsWin.includes(arrCompleteTable[i]))) {
+        const element = document.getElementById(`${arrCompleteTable[i]}`);
+        element.innerText = String.fromCharCode(Math.floor(Math.random() * (90 - 65) + 65));
+    }
+}
+console.log(elementsPositionsWin)
+
+// INSERINDO O EVENTO NAS <td> DE VITÓRIA
+for (let i = 0; i < elementsPositionsWin.length; i++) {
+    const element = document.getElementById(elementsPositionsWin[i]);
+    element.addEventListener('click', youWin)
 }
